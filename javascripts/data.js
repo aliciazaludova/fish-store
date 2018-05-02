67d635974d891e44413132d433971adad0c8877e
Alicia Zaludova <aliciazaludova@gmail.com>
2018-05-01 19:43:19 -0500

    domString built + removed from html

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,9 +1,12 @@
 const loadFishes = require('./fishes');
+const writeFishes = require('./fishDom');
 
 // success function
 // we will console log for now to require acceptance criteria
 const whenFishesLoad = (data) => {
   console.log('data', data);
+  $('#available').append(writeFishes(data.fishes)); // put into the available div. use append and not html so it doesn't overwrite
+  // this is the domFunction that i'm passing fishes array to. this is printToDom
 };
 
 // error function
