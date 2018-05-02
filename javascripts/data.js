978f134dbd78fecb480f849120e7b300b280c395
Alicia Zaludova <aliciazaludova@gmail.com>
2018-05-01 20:52:21 -0500

    added discount function

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,6 +1,7 @@
-const loadFishes = require('./fishes');
+const loadFishes = require('./fishes');  // need loadFishes ajax request. this is used with the success and error functions created here and then passed through
 const writeFishes = require('./fishDom');
 const bindEvents = require('./events');
+const applySale = require('./discount');
 
 // success function
 // we will console log for now to require acceptance criteria
@@ -9,6 +10,7 @@ const whenFishesLoad = (data) => {
   $('#available').append(writeFishes(data.fishes)); // put into the available div. use append and not html so it doesn't overwrite
   // this is the domFunction that i'm passing fishes array to. this is printToDom
   bindEvents.bindEvents();  // this was called and required in main.js but it needs to happen here because the sequence -- domString, then...
+  applySale();
 };
 
 // error function
---
+++ javascripts/discount.js
@@ -0,0 +1,22 @@
+const discount = 0.2;
+
+const applySale = () => {
+  // iterate through jQuery list of dom nodes
+  $('.on-sale').each((i, fishCard) => {
+    // will iterate through all fishCards
+    const basePriceElem = $(fishCard).find('.price');
+    // base price that comes back is html element
+    // we need to get what's inside of it, will come back as text
+    // need to do math on it to reduce to sale price
+    const basePrice = basePriceElem.html() * 1;
+    // .toFixed sets newPrice to two decimals
+    const newPrice = (basePrice * (1 - discount)).toFixed(2);
+    // this will be .8 now. use setter version of .html
+    basePriceElem.html(newPrice);
+  });
+};
+
+// need to require this in data.js because that is where success func is
+
+module.exports = applySale;
+// we want to call it after dom is printed
